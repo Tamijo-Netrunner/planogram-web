0d02bdcb9c6c30c3af390dc8b37e1090f09bdd3a
Fi Fox <39098450+Tamijo-Netrunner@users.noreply.github.com>
2026-08-11 13:12:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def get_format_data():
   response = requests.get(url)
   if response.status_code == 200:
     data = response.json()
-    return id
+    return response.id
   else:
     return "Nope!"
 
