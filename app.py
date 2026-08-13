55577afad2c5d04a243dd8d0ce5339913e1cdf1a
Fi Fox <39098450+Tamijo-Netrunner@users.noreply.github.com>
2026-08-13 18:44:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -25,10 +25,6 @@ if st.button("Search"):
     search_card(queryInput)
 
 
-
-
-
-
-
-
-
+url = "https://api-preview.netrunnerdb.com/api/v3/public/cards?filter[search]=" + searchInput
+response = requests.get(url)
+response
