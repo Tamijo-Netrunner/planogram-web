eedd02299b806d3125cc19177797019939594e7c
Fi Fox <39098450+Tamijo-Netrunner@users.noreply.github.com>
2026-08-13 19:47:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def search_card(searchInput):
   url = "https://api-preview.netrunnerdb.com/api/v3/public/cards?filter[search]=" + searchInput
   response = requests.get(url)
   jsonResponse = response.json()
-  st.write(jsonResponse['data'][0]['attributes']['latest_printing_images']['nrdb_classic']['small'])
+  st.image(jsonResponse['data'][0]['attributes']['latest_printing_images']['nrdb_classic']['small'])
   st.write(jsonResponse)
 
 st.title("Planogram Web")
